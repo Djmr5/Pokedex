487492e651237b57542f7a9186816b9f5baba6eb
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-02-23 20:33:03 -0600

    Pantalla completa y color rojo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button/Button';
 import Top from './components/Top/Top';
 import PokeImage from './components/PokeImage/PokeImage';
 import Bottom from './components/Bottom/Bottom';
 function App() {
 
-  const [contador, setContador]= React.useState(0);
-
-  function aumentar(){
-    setContador(contador+1);
-    console.log(contador);
-  }
-  function disminuir(){
-    setContador(contador-1);
-    console.log(contador);
-  }
   return (
     <div className="App">
       <Top/>
