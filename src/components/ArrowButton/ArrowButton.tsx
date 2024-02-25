b736981788122a2aec2a6fa565541cee0d5d8cd4
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-02-24 19:28:57 -0600

    ArrowButton docs

--- src/components/ArrowButton/ArrowButton.tsx
+++ src/components/ArrowButton/ArrowButton.tsx
@@ -5,7 +5,20 @@ interface ArrowButtonProps {
   direction: string;
 }
 
-export default function ArrowButton(props: ArrowButtonProps) {
+/**
+ * A component that renders a button with an arrow that points
+ * in the direction specified by the direction prop and calls
+ * the function specified by the onClick prop.
+ * 
+ * @param {ArrowButtonProps} props 
+ * @example
+ * <ArrowButton direction="up" onClick={() => console.log('up')} />
+ * 
+ * @author Djmr5
+ * 
+ * @returns {JSX.Element} 
+ */
+export default function ArrowButton(props: ArrowButtonProps): JSX.Element {
   return (
     <button onClick={props.onClick} className="ArrowButton">
       <div
