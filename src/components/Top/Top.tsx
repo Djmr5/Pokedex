9c28927c607c116af7a390d6cc3ae2c1c25727fe
mfnomad <mfnomad@gmail.com>
2024-02-27 17:06:49 +0100

    finished Top component

--- src/components/Top/Top.tsx
+++ src/components/Top/Top.tsx
@@ -1,7 +1,37 @@
+
+import "./Top.css";
+
+
 export default function Top(props: any) {
   return (
-    <div className="mt-3 flex my-2 bg-yellow-400 w-11/12 h-20">
-      <h1 className="text-center">Top</h1>
+    <div className="items-center justify-center mt-3 flex my-2 w-11/12 h-20">
+      
+
+      
+    
+      <div className="outer-circle ml-6">
+      <div className="inner-circle inner-circle-1"></div>
+      <div className="inner-circle inner-circle-2"></div>
+    </div>
+    
+      
+      <img src="https://fontmeme.com/permalink/240227/90d6e73e9572455265e8a2ac57198737.png" alt="pokemon-font" className="mx-auto mr-7" />
+
+      
+      <div className="pokeball ml-4">
+        <div className="half-ball top-half"></div>
+        <div className="black-bar"></div>
+        <div className="center-circle center-circle-outer">
+            <div className="center-circle center-circle-inner"></div>
+        </div>
+        <div className="half-ball bottom-half"></div>
+    </div>
+    
+
+
+   
+
+
     </div>
   );
 }
