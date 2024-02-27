5756abfe72b6c7c3318a9cb33833d1ed498a18c8
Ian Sánchez <a01612486@itesm.mx>
2024-02-27 09:44:43 -0600

    PokeImage
    Integración de imagén y formato de PokeImage

--- src/components/PokeImage/PokeImage.tsx
+++ src/components/PokeImage/PokeImage.tsx
@@ -1,7 +1,9 @@
+import "./PokeImage.css";
 export default function PokeImage(props: any) {
     return (
-      <div className="mb-2 bg-white w-11/12 h-3/5">
+      <div className="mb-2 bg-white w-11/12 h-3/5 border-black border-4 rounded-xl  PokeImage"> 
         <h1>PokeImage</h1>
+
       </div>
     );
 }
