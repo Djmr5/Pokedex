243af40d488a285024a3df73456d618779dd43e6
mfnomad <mfnomad@gmail.com>
2024-02-27 03:37:09 +0100

    template

--- src/components/PokeImage/PokeImage.tsx
+++ src/components/PokeImage/PokeImage.tsx
@@ -1,6 +1,6 @@
 export default function PokeImage(props: any) {
     return (
-      <div>
+      <div className="mb-8 bg-white w-11/12 h-3/5">
         <h1>PokeImage</h1>
       </div>
     );
--- src/components/Top/Top.tsx
+++ src/components/Top/Top.tsx
@@ -1,7 +1,8 @@
 export default function Top(props: any) {
-    return (
-      <div>
-        <h1>Top</h1>
-      </div>
-    );
+  return (
+    <div className="mt-3 flex my-2 bg-yellow-400 w-11/12 h-20">
+      <h1 className="text-center">Top</h1>
+    </div>
+  );
 }
+
