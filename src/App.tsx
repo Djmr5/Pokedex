1bfc9d26b84ab1ad2f1b343cc2ae03898b80efe7
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-03-01 17:52:22 -0600

    Pokemon API

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,34 @@ import Top from './components/Top/Top';
 import PokeImage from './components/PokeImage/PokeImage';
 import Bottom from './components/Bottom/Bottom';
 import { Pokemon } from './models/Pokemon';
+import { PokeApi } from './api/PokeApi';
 function App() {
 
   const [pokemonsNumber, setPokemonsNumber] = React.useState<number>(0);
-  const [pokemon, setPokemon] = React.useState<Pokemon|null>(null);
-  
+  const [pokemon, setPokemon] = React.useState<Pokemon | null>(null);
+
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=10000')
-      .then(response => response.json())
-      .then(data => setPokemonsNumber(data.count));
+    PokeApi.getAllPokemons().then((response) => {
+      setPokemonsNumber(response.data.count);
+    });
+    PokeApi.getPokemonById(1).then((response)=>{
+      setPokemon(response.data as Pokemon);
+    });
   }, []);
 
-  function getPokemon(pokemonId: number) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-      .then(response => response.json())
-      .then(data => setPokemon(data as Pokemon));
-  };
-  
   return (
     <div className="App">
-      <Top/>
-      <PokeImage/>
-      <Bottom/>
-      {/* <button onClick={() => console.log(pokemonsNumber)}>Log Pokemons</button>
-      <button onClick={() => getPokemon(1)}>Get Pokemon</button>
-      <button onClick={() => console.log(pokemon)}>Log Pokemon</button>
-      <button onClick={() => console.log(pokemon?.types)}>Log Pokemon Types</button>
-      <button onClick={() => console.log(pokemon?.types[0].type.name)}>Log Pokemon First Type</button>
-      <button onClick={() => console.log(pokemon?.abilities)}>Log Pokemon Abilities</button>
-      <button onClick={() => console.log(pokemon?.abilities[0].ability.name)}>Log Pokemon First Ability</button>
-      <button onClick={() => console.log(pokemon?.moves)}>Log Pokemon Moves</button>
-      <button onClick={() => console.log(pokemon?.moves[0].move.name)}>Log Pokemon First Move</button> */}
+      <Top />
+      <PokeImage
+        src={pokemon?.sprites.other['official-artwork'].front_default??''}
+        alt={(pokemon?.name ?? '') + ' image'}
+      />
+      <Bottom
+        totalPokemons={pokemonsNumber}
+        pokemonId={pokemon?.id ?? 0}
+        name={pokemon?.name ?? ''}
+        types={pokemon?.types ?? []}
+      />
     </div>
   );
 }
--- src/components/Bottom/Bottom.tsx
+++ src/components/Bottom/Bottom.tsx
@@ -1,11 +1,35 @@
+import React from "react";
 import "./Bottom.css";
 import ArrowButton from "../ArrowButton/ArrowButton";
 import Details from "../Details/Details";
+import { Type } from "../../models/Pokemon";
 
-export default function Bottom(props: any) {
+interface BottomProps {
+    totalPokemons: number;
+    pokemonId: number;
+    name: string;
+    types: Type[];
+}
 
+export default function Bottom(props: BottomProps) {
+
+    const [pokemonId, setPokemonId] = React.useState<number>(props.pokemonId);
+    
     const handleArrowClick = (direction: string) => {
-        console.log(direction);
+        if (direction === 'up') {
+            if (pokemonId < props.totalPokemons) {
+                setPokemonId(pokemonId + 1);
+            } else {
+                setPokemonId(1);
+            }
+        } else {
+            if (pokemonId > 2) {
+                setPokemonId(pokemonId - 1);
+            } else {
+                setPokemonId(props.totalPokemons);
+            }
+        }
+
     }
 
     return (
@@ -13,8 +37,9 @@ export default function Bottom(props: any) {
             <div className="Arrows">
                 <ArrowButton onClick={() => handleArrowClick('up')} />
                 <ArrowButton onClick={() => handleArrowClick('down')} direction="down" />
+                {/* <p>{pokemonId}</p> */}
             </div>
-            <Details />
+            <Details id={props.pokemonId} name={props.name} types={props.types} />
         </div>
     );
 }
