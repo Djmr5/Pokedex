7cd85c70efeb4a8bda50d15dd2a53d00e2f13c26
Caro González <carogonzalezsal@gmail.com>
2024-03-01 16:53:14 -0600

    Cambios.

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -4,7 +4,12 @@ export default function Details(props: any) {
     return (
       <div id="Details">
         <div id="Name">Name</div>
-        <div id="Description">Description</div>
+        <div id="ID">
+          <ul>
+            <li>ID</li>
+            <li>Type</li>
+          </ul>
+        </div>
       </div>
     );
 }
--- src/components/PokeImage/PokeImage.tsx
+++ src/components/PokeImage/PokeImage.tsx
@@ -1,7 +1,7 @@
 import "./PokeImage.css";
 export default function PokeImage(props: any) {
     return (
-      <div className="mb-2 bg-white w-11/12 h-3/5 border-black border-4 rounded-xl  PokeImage"> 
+      <div className="bg-white w-11/12 h-3/5 border-black border-4 rounded-xl  PokeImage"> 
         <h1>PokeImage</h1>
 
       </div>
